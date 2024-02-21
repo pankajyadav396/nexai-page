c4785ef9ded7bcc338fdef3ff05c06231e923e31
Your Name <you@example.com>
2024-02-21 10:22:42 +0530

    commit

--- src/components/Commonbtn.jsx
+++ src/components/Commonbtn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Commonbtn = (props) => {
     return (
-        <button data-aos='zoom-in' className='bg-[#02CDCF] hover:bg-[#040404] duration-300 hover:border-[white] btn-hover hover:border-bottom-[2px] text-black p-[18px_32px] ff-recharge font-bold border-none rounded-xl hover:text-white
+        <button className='bg-[#02CDCF] duration-300 hover:bg-[#040404] hover:border-[white] btn-hover hover:border-[2px] text-black p-[18px_32px] ff-recharge font-bold border-none rounded-xl hover:text-white
         '>{props.text}</button>
     )
 }
