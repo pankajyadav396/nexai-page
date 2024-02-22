9fa42b1ed7d30b2c98f464ce6f2decac652fcc4c
Your Name <you@example.com>
2024-02-22 16:28:52 +0530

    commit updates

--- src/components/Threesliders.jsx
+++ src/components/Threesliders.jsx
@@ -53,13 +53,13 @@ const Threesliders = () => {
     };
     return (
         <>
-            <h2 data-aos="fade-up" className='text-[#02CDCF] lg:mt-[150px] md:mt-[100px] mt-[80px] xl:mt-[200px] font-bold text-[32px] sm:text-[40px] text-center md:text-[30px] lg:text-[40px] mb-4 ff-recharge leading-[50.4px]' id='about'>NexAi does all this. And more.</h2>
-            <p data-aos="fade-up" className='text-lightGrey font-normal mb-[90px] text-[14px] max-w-[957px] mx-auto text-center sm:text-[16px] md:text-[14px] lg:text-[16px] mb-[6px] ff-["poppins"]'>Nexai is an advanced home assistant that will revolutionize the way we interact with our smart devices at home. Powered by sophisticated artificial intelligence, it leverages blockchain technology to offer a secure and efficient user experience.</p>
+            <h2 data-aos="fade-up" className='text-[#02CDCF] lg:mt-[150px] md:mt-[100px] mt-[80px] xl:mt-[200px] font-bold text-[28px] sm:text-[40px] text-center md:text-[30px] lg:text-[40px] mb-4 ff-recharge leading-[50.4px]' id='about'>NexAi does all this. And more.</h2>
+            <p data-aos="fade-up" className='text-lightGrey font-normal mb-[90px] text-[14px] max-w-[957px] mx-auto text-center sm:text-[16px] md:text-[14px] lg:text-[16px] font-[poppins]'>Nexai is an advanced home assistant that will revolutionize the way we interact with our smart devices at home. Powered by sophisticated artificial intelligence, it leverages blockchain technology to offer a secure and efficient user experience.</p>
             <Slider {...settings}>
                 {threeSlider.map((obj, index) => {
                     return (<div data-aos="zoom-in" className='bg-[url(./assets/images/webp/bg-threeslider.webp)] flex flex-col items-center justify-center bg-no-repeat bg-100-100 bg-center h-[220px] w-[340px]'>
                         <div className='justify-center items-center flex flex-col w-full h-full'>
-                            <p className='text-white ff-recharge font-bold leading-[32px] max-w-[212px] text-center mx-auto text-[20px]'>{obj.slideTitle}</p>
+                            <p className='text-white ff-recharge font-bold leading-[32px] max-w-[212px] text-center mx-auto text-[16px] sm:text-[20px]'>{obj.slideTitle}</p>
                         </div>
                     </div>)
                 })}
@@ -68,7 +68,7 @@ const Threesliders = () => {
                 {threeSlider.map((obj, index) => {
                     return (<div data-aos="zoom-in" className='bg-[url(./assets/images/webp/bg-threeslider.webp)] flex flex-col items-center justify-center bg-no-repeat bg-100-100 bg-center h-[220px] w-[340px]'>
                         <div className='justify-center items-center flex flex-col w-full h-full'>
-                            <p className='text-white ff-recharge font-bold leading-[32px] max-w-[212px] text-center mx-auto text-[20px]'>{obj.slideTitle}</p>
+                            <p className='text-white ff-recharge font-bold leading-[32px] max-w-[212px] text-center mx-auto text-[16px] sm:text-[20px]'>{obj.slideTitle}</p>
                         </div>
                     </div>)
                 })}
@@ -77,7 +77,7 @@ const Threesliders = () => {
                 {threeSlider.map((obj, index) => {
                     return (<div data-aos="zoom-in" className='bg-[url(./assets/images/webp/bg-threeslider.webp)] flex flex-col items-center justify-center bg-no-repeat bg-100-100 bg-center h-[220px] w-[340px]'>
                         <div className='justify-center items-center flex flex-col w-full h-full'>
-                            <p className='text-white ff-recharge font-bold leading-[32px] max-w-[212px] text-center mx-auto text-[20px]'>{obj.slideTitle}</p>
+                            <p className='text-white ff-recharge font-bold leading-[32px] max-w-[212px] text-center mx-auto text-[16px] sm:text-[20px]'>{obj.slideTitle}</p>
                         </div>
                     </div>)
                 })}
