0afbc1aed0c837c961b69937a6cd370df1b04712
Your Name <you@example.com>
2024-02-27 12:40:44 +0530

    commit-change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
               Etiam viverra nec libero a. A id id tempus molestie sed. Fringilla
               odio.
             </p>
-            <div data-aos="fade-up" data-aos-delay="3000" className='block'>
+            <div data-aos="zoom-in" data-aos-delay="3000" className='block'>
               <Commonbtn className="hover:shadow-[inset_200px_0_0_0_black]"
                 text="Know More"
               />
